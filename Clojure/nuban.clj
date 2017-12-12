713654196b14e4e9cc82950213d5736c67642584
Efe Ariaroo <efeariaroo@gmail.com>
2017-12-12 23:55:51 +0100

    Now in testing phase ... not perfect yet

--- Clojure/nuban.clj
+++ Clojure/nuban.clj
@@ -1,4 +1,4 @@
-(require '[clojure.string :as str])
+; (require '[clojure.string :as str])
 
 ;;; How to run
 ;;; On your terminal cd into the directory with the nuban.clj file
@@ -13,8 +13,8 @@
 (defn validate-nuban [bank-code account-number]
   (if (or (not (string? bank-code)) (not (string? account-number)))
     (throw (Exception. "Bank Code and Account Number must be strings"))
-    (if (or (not (= (count bank-code) 3) 
-             (not (= (count account-number) 10))))
+    (if (or (not (= (count bank-code) 3)) 
+             (not (= (count account-number) 10)))
       (throw (Exception. "Bank code should be 3 characters long
                and account number should be 10 characters long"))
       (let [check-digit (.charAt account-number 9)
@@ -22,15 +22,19 @@
             account-serial-num (.substring account-number 0 9)
             nuban-account-format (str bank-code account-serial-num)
             nuban-chars (vec (seq nuban-account-format))]
-        (loop [check-sum 0
-               curr-index 0
-               curr-nuban-char (nuban-chars 0)]
+        (do 
+          (print (str "account-serial-num: " account-serial-num))
+          (print (str "nuban-account-format: " nuban-account-format))
+          (print (str "nuban-account-format: " nuban-account-format))
+          (loop [check-sum 0
+                  curr-index 0
+                  curr-nuban-char (nuban-chars 0)]
           (if (= curr-index (count nuban-chars))
             (recur 
               (+ check-sum (* curr-nuban-char (nuban-chars curr-index)))
               (inc curr-index)
               (nuban-chars (inc curr-index)))
             (let [validated-check-digit (- 10 (mod check-sum 10))
-                  final-validated-check-digit (if (= 0 validated-check-digit) 
+                  final-validated-check-digit (if (= 10 validated-check-digit) 
                                                 0 validated-check-digit)]
-              (= check-digit final-validated-check-digit))))))))
+              (= check-digit final-validated-check-digit)))))))))
